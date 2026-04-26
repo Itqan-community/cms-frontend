ec381ca459bc4fc35e4f5b264f5a9d381da81e51
Amr Bendary <amr.bendary.business@gmail.com>
2026-04-26 12:20:43 +0300

    feat(core): add local env

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  API_BASE_URL: 'https://elated-lugged-frays.ngrok-free.dev/cms-api',
+  API_DOCS_URL: 'https://elated-lugged-frays.ngrok-free.dev/docs',
+  ADMIN_API_BASE_URL: 'https://elated-lugged-frays.ngrok-free.dev/portal',
+  sentryDsn:
+    'https://dce9e26dda3c82231e15b1c029696db0@o4510669335232512.ingest.de.sentry.io/4511206115508304',
+};
